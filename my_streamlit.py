dd4ab93064385b74c64e1471b3d94751950ddcf3
uj7788-del <uj7788@gmail.com>
2026-05-19 10:33:37 +0900

    Add files via upload

---
+++ my_streamlit.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+# 타이틀 텍스트 출력
+st.title('첫번째 웹 어플 만들기 🤙😁🤙🎮🕹️👾')
+
+'''
+# 이것이 제목 :smile: :sunglasses:
+이것은 그냥 문자열 
+### 지금부터는 코드 
+```python
+import streamlit as st
+import pandas
+
+a=3
+b=5
+print(a+b)
+```
+## 이것이 부제목 :smile: :sunglasses:
+'''
+456 
+
+streamlit.caption('이것이 캡션이다')
